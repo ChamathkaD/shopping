7f03cd7a3ea5b230f7d51ebfbf78103d0098c3f6
ChamathkaDissanayaka <chamathka.dissa@gmail.com>
2021-10-31 20:16:11 +0530

    create account.blade.php

---
+++ resources/views/profile/account.blade.php
@@ -0,0 +1,10 @@
+
+<x-app-layout>
+
+    @if(\Laravel\Fortify\Features::canUpdateProfileInformation())
+        @livewire('profile.update-profile-information-form')
+    @endif
+
+
+
+</x-app-layout>
